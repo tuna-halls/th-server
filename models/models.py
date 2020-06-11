c2c9e600832835a6e4948f856623577fd504eaed
gldev <gldev@Edgars-MacBook-Pro.local>
2020-06-10 22:19:57 -0500

    Model draft

---
+++ models/models.py
@@ -0,0 +1,75 @@
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import DateTime
+from sqlalchemy import Enum
+from sqlalchemy.ext.declarative import declarative_base
+
+from .enums import HallState
+from .enums import HallSecurityMode
+from .enums import HallLogMode
+from .enums import FeatureType
+from .enums import Role
+from .enums import Presence
+
+import os
+
+db_engine = os.environ.get("DB_ENGINE", "sqlite:///:memory:")
+
+engine = create_engine(db_engine, echo=True)
+Base = declarative_base()
+
+
+
+class TimestampedMixin(object):
+	created_at = Column(DateTime, default=datetime.now)
+	updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
+
+class User(TimestampedMixin, Base):
+	__tablename__ = "user"
+	id = Column(Integer, primary_key=True)
+	email = Column(String, unique=True)
+	password = Column(String)
+	display_name = Column(String)
+	profile_image = Column(String)
+	member = relationship("HallMember", back_populates="user")
+
+
+class Hall(TimestampedMixin, Base):
+	__tablename__ = "hall"
+	id = Column(Integer, primary_key=True)
+	email = Column(String, unique=True)
+	slug = Column(String)
+	state = Column(Enum(HallState))
+	title = Column(String)
+	description = Column(String)
+	profile_image = Column(String)
+	security_mode = Column(Enum(HallSecurityMode))
+	log_mode = Column(Enum(HallLogMode))
+	share_link = Column(String)
+	expires_at = Column(DateTime)
+	features = relationship("HallFeature", back_populates="hall")
+	members = relationship("HallMember")
+
+
+class HallFeature(TimestampedMixin, Base):
+	__tablename__ = "hallfeature"
+	id = Column(Integer, primary_key=True)
+	hall_id = Column(Integer, ForeignKey('hall.id'))
+	feature_type = Column(Enum(FeatureType))
+	settings = Column(String)
+
+
+
+class HallMember(TimestampedMixin, Base):
+	__tablename__ = "hallmember"
+	id = Column(Integer, primary_key=True)
+	hall_id = Column(Integer, ForeignKey('hall.id'))
+	user_id = Column(Integer, ForeignKey('user.id'))
+	presence = Column(Enum(Presence))
+	display_name = Column(String)
+	profile_image = Column(String)
+	role = Column(Enum(Role))
